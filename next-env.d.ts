689a65ecc0851824405b92f483b08628473ef22e
Jonatan Lins <jonatanpinheiro99@gmail.com>
2020-07-15 17:27:33 -0300

    added image types

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,9 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.png";
+declare module "*.svg";
+declare module "*.gif";
+declare module "*.webp";
