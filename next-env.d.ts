00e6ea4eda0e5e1a3f47ae6192ce1562d9f99e2e
Jonatan Lins <jonatanpinheiro99@gmail.com>
2020-07-15 18:48:51 -0300

    fiz até um pull request pra corrigir a porra do bug mas aquela merda já tinha sido corrigida na versão beta vai tomarno cu que se foda vou jogar gta

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,37 +1,3 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-
-declare module "*.jpg" {
-  const value: string;
-  export = value;
-}
-
-declare module "*.jpeg" {
-  const value: string;
-  export = value;
-}
-
-declare module "*.png" {
-  const value: string;
-  export = value;
-}
-
-declare module "*.svg" {
-  const value: string;
-  export = value;
-}
-
-declare module "*.webp" {
-  const value: string;
-  export = value;
-}
-
-declare module "*.gif" {
-  const value: string;
-  export = value;
-}
-
-declare module "*.ico" {
-  const value: string;
-  export = value;
-}
+/// <reference types="optimized-images-loader" />
